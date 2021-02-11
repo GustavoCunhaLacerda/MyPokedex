74c895404515e5b94b5d202b6d8e95f46e775332
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2021-02-11 17:46:23 -0300

    Routes && Structure

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,20 @@
+import './Content.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../../views/Home';
+import SingleCard from '../../views/SingleCard';
+
+export default function Content(props) {
+    return (
+        <main className="Content">
+            <Switch>
+                <Route path="/single-card">
+                    <SingleCard></SingleCard>
+                </Route>
+                <Route path="/">
+                    <Home></Home>
+                </Route>
+            </Switch>
+        </main>
+    );
+};
