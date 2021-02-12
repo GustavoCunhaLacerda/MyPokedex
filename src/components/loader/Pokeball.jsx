79952dd6a7369cdb8d9caf253a1270b7c27468e7
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2021-02-12 15:23:18 -0300

    Loader

---
+++ src/components/loader/Pokeball.jsx
@@ -0,0 +1,10 @@
+import './Pokeball.css';
+import React from 'react';
+
+export default function Pokeball() {
+    return (
+        <div className="Pokeball">
+            <img src="./pokeball.svg" alt="" />
+        </div>
+    );
+};
